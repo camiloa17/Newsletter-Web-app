be7a4e34e8f4fe3e4294453a137b75b1995b8bb2
camilo dominguez <camiloa17@gmail.com>
2019-03-09 18:22:50 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function(req,res){
+
+    res.sendFile(__dirname + "/signup.html");
+});
+
+
+app.post("/",function(req,res){
+
+    var firstName = req.body.firstName;
+    var lastName = req.body.lastName;
+    var email = req.body.emailAddress;
+
+    var data = {
+        members: [
+            {
+                email_address:email, 
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME:lastName,
+                }
+            }
+        ]
+    };
+
+   var jsonData = JSON.stringify(data);
+    console.log(firstName, lastName, email);
+
+    var options = {
+        url: "https://us16.api.mailchimp.com/3.0/lists/040c7424e5",
+        method:"POST",
+        headers: {
+            "Authorization": "camilo1 c0377555dea6ab01fabf7c86cb6467af - us16"
+        },
+        body: jsonData,
+       
+    };
+
+    request(options, function(error,response,body){
+        if (error){
+            res.sendFile(__dirname + "/failure.html");
+            console.log(error);
+            
+        }else {
+            console.log(response.statusCode);
+            if(response.statusCode === 200){
+                res.sendFile(__dirname + "/success.html");
+            }else {
+                res.sendFile(__dirname + "/failure.html");
+            }
+            
+            
+        }
+    });
+});
+
+app.post("/failure", function(req, res){
+    res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000 , function(){
+    console.log("Server started on host 3000");
+    
+});
+
